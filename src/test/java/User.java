e6817dd4d961b7602abc816101551849845f749f
Ces <ces@Zipcoders-MacBook-Pro-42.local>
2023-12-07 13:26:31 -0500

    created classes

---
+++ src/test/java/User.java
@@ -0,0 +1,27 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class User {
+    String speak = "";
+
+
+    @Test
+    public void TestUserPetCountResponse() {
+        //Given
+        User user = new User(); // Create a user so they can respond
+        // When
+        // ask user how many pets do you have?
+        //Then
+        Assert.assertTrue();
+    }
+
+    @Test
+    public void TestUserPetList() {
+        //Given
+        User user = new User(); // Create a user so they can list each of their pets
+        //When
+        //ask user what each kind of pet they have
+        //Then
+        // generate code that checks to see if user responce is true
+    }
+}
